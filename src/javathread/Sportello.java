bfa0462a2f40a042162fec21d353cdba1f75fc81
pogliani.mattia <pogliani.mattia@TAVE-PC00.OSDBSESTO.local>
2020-12-01 13:42:47 +0100

    add synchronized method example (conto, sportello, genitore1&2)

---
+++ src/javathread/Sportello.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathread;
+
+/**
+ *
+ * @author pogliani.mattia
+ */
+public class Sportello implements Runnable{
+    
+    Conto conto;
+
+    public Sportello(Conto conto) {
+        this.conto = conto;
+    }      
+
+    @Override
+    public void run() {
+        this.versa(500);
+    }
+    
+    public void versa(int somma){
+        conto.versa(somma);
+        //System.out.println("conto: " + conto.saldo());
+    }
+    
+}
